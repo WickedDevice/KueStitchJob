245fd747254fa43ef6ac6bc13474bef06f51fa31
Victor Aprea <victor.aprea@wickeddevice.com>
2016-11-12 04:24:53 +0000

    framing code with some comments

--- stitchjob.js
+++ stitchjob.js
@@ -21,6 +21,16 @@ queue.process('stitch', (job, done) => {
   getDirectories(job.data.save_path).forEach( (dir) => {
     fs.closeSync(fs.openSync(`${job.data.save_path}/${dir}.csv`, 'w'));
   });
+
+  // 2. for each folder in save_path, analyze file 1.json to infer the type of Egg
+  //    model, generate an appropriate header row and append it to the csv file, 
+  //    and create a map of topic string to CSV index based on the determined model
+  
+
+  // 3. for each folder in save_path, list the files in that folder
+  //    load them into memory one at a time, and process records in each file
+  //    generating one csv record at a time and appending to the csv file
+  //    progressively as you go
   
 
   done();
