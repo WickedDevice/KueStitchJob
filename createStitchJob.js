5e131d33a8cb8ea79de3ffaa05a3ba0f9740a0dc
Victor Aprea <victor.aprea@wickeddevice.com>
2018-07-25 17:27:49 -0400

    add stitchjob creatrion script

---
+++ createStitchJob.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var path = require('path');        
+var kue = require('kue')
+  , queue = kue.createQueue();
+
+let job = {
+  data: {
+    original_serials: ["egg0080425108080130"],
+    original_url: "blah",
+    save_path : "/home/wd/dev/KueStitchJob/tmp",
+    user_id: "blah",
+    email: "blah",
+    utcOffset: -4,
+    compenasted: true,
+    instantaneous: false,
+    zipfilename: "blah",
+    bypassjobs: [],
+    stitch_format: "csv"
+  }
+};
+                   
+        let getDirectories = (srcpath) => {
+          return fs.readdirSync(srcpath).filter( (file) => {
+            return fs.statSync(path.join(srcpath, file)).isDirectory();
+          });
+        }
+
+        let directories = getDirectories(job.data.save_path);
+
+        let job2 = queue.create('stitch', {
+            title: 'stitching data for ' + job.data.original_serials[0]
+          , save_path: job.data.save_path
+          , original_serials: job.data.original_serials.slice()
+          , original_url: job.data.original_url
+          , serials: directories
+          , user_id: job.data.user_id
+          , email: job.data.email
+          , compensated: job.data.compensated
+          , instantaneous: job.data.instantaneous
+          , utcOffset: job.data.utcOffset
+          , zipfilename: job.data.zipfilename
+          , bypassjobs: job.data.bypassjobs ? job.data.bypassjobs.slice() : []
+          , stitch_format: job.data.stitch_format
+        })
+        .priority('high')
+        .attempts(1)
+        .save();
+
+process.once( 'uncaughtException', function(err){
+  console.error( 'Something bad happened: ', err );
+  queue.shutdown( 1000, function(err2){
+    console.error( 'Kue shutdown result: ', err2 || 'OK' );
+    process.exit( 0 );
+  });
+});
