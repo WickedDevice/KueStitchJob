66a4142df18eca53e4160c4a7eafc197840c64fd
Victor Aprea <victor.aprea@wickeddevice.com>
2018-07-25 23:40:13 -0400

    remove weird voc case

--- stitchjob.js
+++ stitchjob.js
@@ -702,7 +702,7 @@ const getEggModelType = (dirname, extantTopics) => {
     case 0b1100: return 'model B'; // so2 + o3
     case 0b10000: return 'model C'; // NOTE: there is actually a conflict between C and N here
     case 0b100000: return 'model D'; // co2
-    case 0b1100000: // NOTE: this is just for data recorded before 3/27/2018
+    // case 0b1100000: // NOTE: this is just for data recorded before 3/27/2018
     case 0b1000000: return 'model E'; // voc
     case 0b110000: return 'model G'; // co2 + particulate
     case 0b1001: return 'model J'; // Jerry model no2 + o3
