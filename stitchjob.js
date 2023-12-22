24a912c6877c918ebe41418acd6bdef519dff50e
Victor Aprea <victor.aprea@wickeddevice.com>
2023-12-22 14:11:47 -0500

    debug

--- stitchjob.js
+++ stitchjob.js
@@ -628,9 +628,8 @@ const addMessageToRecord = (message, model, compensated, instantaneous, record, 
       const offset = offsetByModel[model];
       if (isNumeric(offset)) {
         record[offset] = valueOrInvalid(message['compensated-value']);      
-        if (record[offset] === '---') {
-          console.log(JSON.stringify(message, null, 2));
-        }
+        console.log(JSON.stringify(message, null, 2));
+        console.log(JSOn.stringify(record));
       } else {
         console.error(`SO2 offset not established for Model "${model}"`);
       }
