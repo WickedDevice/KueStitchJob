86caa4d88018c0095adae7959983b28482821fee
Victor Aprea <victor.aprea@wickeddevice.com>
2024-10-24 20:41:46 -0400

    update mappings

--- mappings.js
+++ mappings.js
@@ -37,9 +37,9 @@ const TOPIC_TO_MAPPING_DATA = {
     {csvHeading: 'temperature', jsonValueField: 'converted-value', jsonUnitsField: 'converted-units', influxValueField: 'temperature', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
   ],
   temperature3: [
-    {csvHeading: 'temperature1', jsonValueField: 'temperature1', jsonUnitsField: 'units', influxValueField: 'temperature_1', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
-    {csvHeading: 'temperature2', jsonValueField: 'temperature2', jsonUnitsField: 'units', influxValueField: 'temperature_2', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
-    {csvHeading: 'temperature3', jsonValueField: 'temperature3', jsonUnitsField: 'units', influxValueField: 'temperature_3', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
+    {csvHeading: 'temperature_1', jsonValueField: 'temperature_1', jsonUnitsField: 'units', influxValueField: 'temperature_1', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
+    {csvHeading: 'temperature_2', jsonValueField: 'temperature_2', jsonUnitsField: 'units', influxValueField: 'temperature_2', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
+    {csvHeading: 'temperature_3', jsonValueField: 'temperature_3', jsonUnitsField: 'units', influxValueField: 'temperature_3', influxUnitsfield: 'temperature_units', defaultUnits: 'degC'},
   ],
   pressure: [
     {csvHeading: 'pressure', jsonValueField: 'pressure', jsonUnitsField: 'pressure-units', influxValueField: 'pressure', influxUnitsfield: 'pressure_units', defaultUnits: 'Pa'},
